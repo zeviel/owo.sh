3d064b09db94b37d62edf0a48b583d31fa4daec0
Katlyn Lorimer <katlyn@katlyn.dev>
2023-04-03 22:23:56 -0800

    fix: update curl requests to use v2 api

--- src/owo.sh
+++ src/owo.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-api="https://owo.vc"
+api="https://owo.vc/api/v2"
 
 function shorten_url() {
-	curl --request POST \
-		--url "$api/generate" \
+	curl -v --request POST \
+		--url "$api/link" \
 		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
 		--header "accept: application/json"	\
 		--header "content-type: application/json" \
 		--data '{
 			"link": "'$1'",
 			"generator": "'$2'",
-			"preventScrape": "'$3'",
-			"owoify": "'$4'",
+			"metadata": "'$3'"
 		}'
 }
 
 function get_url_info() {
 	curl --request GET \
-		--url "$api/info/$1" \
+		--url "$api/link/$1" \
 		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
 		--header "accept: application/json"	\
 		--header "content-type: application/json"
